8d8c157e96a15acd1425888e6c4d5caaa7415aac
doantaa <doanta7aloycius@gmail.com>
2023-01-24 01:03:58 +0700

    Cleaning add user

--- mycinema/src/main/java/com/tubes/ticketing/DataBase.java
+++ mycinema/src/main/java/com/tubes/ticketing/DataBase.java
@@ -6,38 +6,42 @@ import java.util.ArrayList;
 public class DataBase {
 
 
-    final static private String[]
+     public static String[][] userList=  {
+            {
+                    "Ananta Wardana Hartanta",
+                    "admin",
+                    "ananta@ittelkom-pwt.ac.id",
+                    "admin"},
 
-            user1 = {
-            "Ananta Wardana Hartanta",
-            "admin",
-            "ananta@ittelkom-pwt.ac.id",
-            "admin"},
-
-            user2 = {
+            {
                     "IT Telkom",
                     "ittelkompurwokerto",
                     "ittelkom@gmail.com",
                     "ittp"},
 
-            user3 = {
+            {
                     "Admin Kasir",
                     "adminkasir",
                     "adminkasir@ittelkom-pwt.ac.id",
-                    "adminkasir"
-            };
+                    "adminkasir"},
+            {
+                    "Admin",
+                    "",
+                    "admin@moviesystem.ac.id",
+                    ""}
+    };
 
 
     static private ArrayList<String[]> user = new ArrayList<>(5);
     
     
     static public String getQuery(int empIndex, String datafield){
-        
-        user.add(user1);
-        user.add(user2);
-        user.add(user3);
 
-        
+        for (String[] u: userList ){
+            user.add(u);
+        }
+
+
         if(datafield == "fname"){
             return user.get(empIndex)[0];
         }
